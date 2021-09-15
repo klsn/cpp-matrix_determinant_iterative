c4c0675a0a7ca849becda70ced3b0344872eca7f
klsn <git@su-shiki.com>
2021-09-15 15:28:04 +0900

    added ansLog

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int n;
+	double ans=1, temp;
+	int ansLog=0, n, tempInt;
 	cin >> n;
 	vector<vector<double>> matrix (n, vector<double>(n));
 	for (int i=0;i<n;i++)
 		for (int j=0;j<n;j++){
 			cin >> matrix.at(i).at(j);
 		}
-	double temp;
+
 	for (int i=0;i<n-1;i++){
 		for (int j=i+1;j<n;j++){
 				temp = matrix.at(j).at(i) / matrix.at(i).at(i);
-			for (int k=i;k<n;k++){  // 0 1 0  
+			for (int k=i;k<n;k++){  
 				matrix.at(j).at(k) -= matrix.at(i).at(k) * temp;
 				}
 			}
@@ -27,10 +28,17 @@ int main() {
 	}
 
 
-	double ans=1;
 	for (int i=0;i<n;i++) {
 		ans *= matrix.at(i).at(i);
-	
-	cout << ans <<endl;
-}
+		if (ans != 0){
+			tempInt = log(abs(ans))/log(10);
+			if (tempInt !=0){
+				ansLog += tempInt;
+				ans/= pow(10,tempInt);
+			}
+		}
+
+	}		
+	cout << ans <<" \\times 10^ "<< ansLog <<endl;
+
 }
